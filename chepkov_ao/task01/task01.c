13e58d031444b109a234c1a7057b6136e6028aa0
Alexander <dickmhathair@gmail.com>
2021-03-22 21:39:15 +0300

    Сделал task01.c

---
+++ chepkov_ao/task01/task01.c
@@ -0,0 +1,47 @@
+#include "stdio.h"
+
+int fizzbuzz(int a, int b) {
+	
+	if ((a < 0) || (b < 0))
+		printf(" cannot be done!\n");
+	else {
+		printf(": \n");
+		for (int i = a; i < (a + b); i++) {
+				
+			if (!(i % 3) && !(i % 5) && (i != 0))
+				printf("Fizz Buzz");
+			else if (!(i % 3) && (i != 0))
+				printf("Fizz");
+			else if (!(i % 5) && (i != 0))
+				printf("Buzz");
+			else
+				printf("%d", i);
+				
+			if (i == (a + b - 1))
+				printf(".\n");
+			else
+				printf(", ");
+				
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	
+	int a, b = 0;
+	
+	printf("\nEnter values 'a b', where:\n");
+	printf("a - first number in sequence;\n");
+	printf("b - string size:\n\n");
+	
+	scanf("%d%d", &a, &b);
+	
+	printf("\nYour sequence");
+	
+	fizzbuzz(a, b);
+	
+	return 0;
+}
+		
+			
